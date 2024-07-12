135f047e8a29b422b949dd6b185da20e20fae41a
Jesus Sanz <jesus@jsanz.dev>
2024-07-12 22:09:27 +0200

    chore: Add member list to app and style updates

--- 4 - Frameworks/01 - React/My-First-App/src/app.tsx
+++ 4 - Frameworks/01 - React/My-First-App/src/app.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 
+interface Member {
+  id: number;
+  login: string;
+  avatar_url: string;
+}
+
 export const App = () => {
-  return <div>Hello, World!</div>;
+  const [members, setMembers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.github.com/orgs/lemoncode/members")
+      .then((response) => response.json())
+      .then((result) => setMembers(result));
+  }, []);
+
+  return (
+    <div className="user-list-container">
+      <span className="header">Avatar</span>
+      <span className="header">Login</span>
+      <span className="header">ID</span>
+      {members.map((member: Member) => (
+        <React.Fragment key={member.id}>
+          <img src={member.avatar_url} />
+          <div>{member.login}</div>
+          <div>{member.id}</div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 };
