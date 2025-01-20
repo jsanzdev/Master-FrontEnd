d712952fa49c0b6d0858a8708fa4ff39ff7e1d65
Jesus Sanz <jesus@jsanz.dev>
2025-01-20 16:39:36 +0100

    Module 3 - Bundlers  - Webpack Excersize.

---
+++ 3 - Bundlers/Ejercicios/Webpack/webpack.dev.js
@@ -0,0 +1,51 @@
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+import Dotenv from "dotenv-webpack";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
+export default merge(common, {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                exportLocalsConvention: "camelCase",
+                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    port: 8080,
+    devMiddleware: {
+      stats: "errors-only",
+    },
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.dev.env",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      reportFilename: "bundle-report.html",
+      openAnalyzer: true,
+    }),
+  ],
+});
