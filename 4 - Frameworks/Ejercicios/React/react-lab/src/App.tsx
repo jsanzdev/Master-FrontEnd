6a2bfe743f075a0f5f6f1cee4e437490cdb2882b
Jesus Sanz <jesus@jsanz.dev>
2025-01-27 18:59:01 +0100

    React Lab - Added Header.

--- 4 - Frameworks/Ejercicios/React/react-lab/src/App.tsx
+++ 4 - Frameworks/Ejercicios/React/react-lab/src/App.tsx
@@ -1,17 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FC } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
 
-export const App = () => {
+export const App: FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-      </Routes>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              React Lab
+            </Typography>
+            <Button color="inherit" component={Link} to="/login">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/list">
+              Github Search
+            </Button>
+            <Button color="inherit" component={Link} to="/list">
+              Rick and Morty
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+        </Routes>
+      </Box>
     </Router>
   );
 };
