c699d1ae33c5813427b0f9b339edfb526b8553eb
Jesus Sanz <jesus@jsanz.dev>
2025-02-28 17:00:29 +0100

    Testing Lab - Added test for Spinner Component.

--- 5 - Testing/Ejercicios/Testing-Lab/src/common/components/spinner/spinner.component.tsx
+++ 5 - Testing/Ejercicios/Testing-Lab/src/common/components/spinner/spinner.component.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { usePromiseTracker } from 'react-promise-tracker';
-import { Modal } from '@mui/material';
-import Loader from 'react-spinners/ScaleLoader';
-import * as classes from './spinner.styles';
+import React from "react";
+import { usePromiseTracker } from "react-promise-tracker";
+import { Modal } from "@mui/material";
+import Loader from "react-spinners/ScaleLoader";
+import * as classes from "./spinner.styles";
 
 export const SpinnerComponent: React.FunctionComponent = () => {
   const { promiseInProgress } = usePromiseTracker();
   return (
     <Modal open={promiseInProgress} className={classes.modal}>
-      <div className={classes.loaderContainer}>
+      <div className={classes.loaderContainer} data-testid="loader-container">
         <Loader />
       </div>
     </Modal>
