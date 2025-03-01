23994a1fc54f40c9198ead0e28127aef9fb96e12
Jesus Sanz <jesus@jsanz.dev>
2025-03-01 15:01:39 +0100

    Fixing CI and Test for Unit Test and E2E

--- 5 - Testing/Ejercicios/Testing-Lab/config/test/config.ts
+++ 5 - Testing/Ejercicios/Testing-Lab/config/test/config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     restoreMocks: true,
-    environment: 'jsdom',
-    setupFiles: ['./config/test/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./config/test/setup.ts"],
+    include: ["./src/**/*.spec.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: [...configDefaults.exclude, "e2e/*"],
   },
 });
