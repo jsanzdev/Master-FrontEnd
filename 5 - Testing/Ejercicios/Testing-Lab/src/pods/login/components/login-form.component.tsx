00ebf71337d76dbe5ab324c187181adb6a118e32
Jesus Sanz <jesus@jsanz.dev>
2025-03-01 13:09:00 +0100

    Testing Lab - E2E implemented with Playwright.

--- 5 - Testing/Ejercicios/Testing-Lab/src/pods/login/components/login-form.component.tsx
+++ 5 - Testing/Ejercicios/Testing-Lab/src/pods/login/components/login-form.component.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Formik, Form } from 'formik';
-import { Button } from '@mui/material';
-import { TextFieldComponent } from '#common/components';
-import { Login, createEmptyLogin } from '../login.vm';
-import { formValidation } from '../login.validation';
-import * as classes from './login-form.styles';
-import { literals } from '#core/i18n';
+import React from "react";
+import { Formik, Form } from "formik";
+import { Button } from "@mui/material";
+import { TextFieldComponent } from "#common/components";
+import { Login, createEmptyLogin } from "../login.vm";
+import { formValidation } from "../login.validation";
+import * as classes from "./login-form.styles";
+import { literals } from "#core/i18n";
 
 interface Props {
   onLogin: (login: Login) => void;
@@ -20,23 +20,26 @@ export const LoginFormComponent: React.FunctionComponent<Props> = (props) => {
       validate={formValidation.validateForm}
     >
       {() => (
-        <Form className={classes.root}>
+        <Form className={classes.root} data-testid="login-form">
           <TextFieldComponent
             name="user"
             label={`${literals.components.fields.user} *`}
             variant="outlined"
+            inputProps={{ "data-testid": "user-input" }}
           />
           <TextFieldComponent
             name="password"
             label={`${literals.components.fields.password} *`}
             type="password"
             variant="outlined"
+            inputProps={{ "data-testid": "password-input" }}
           />
           <Button
             type="submit"
             variant="contained"
             color="primary"
             className={classes.submit}
+            data-testid="login-button"
           >
             Login
           </Button>
