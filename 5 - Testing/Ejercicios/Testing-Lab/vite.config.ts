0409d50f23d5ec4b034e1c1319f5f8485f6b9b04
Jesus Sanz <jesus@jsanz.dev>
2025-03-01 13:42:44 +0100

    Testing Lab, Fixing Vitest.

--- 5 - Testing/Ejercicios/Testing-Lab/vite.config.ts
+++ 5 - Testing/Ejercicios/Testing-Lab/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    restoreMocks: true,
+    include: ["./src/**/*.spec.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: [...configDefaults.exclude, "e2e/*"],
+  },
 });
